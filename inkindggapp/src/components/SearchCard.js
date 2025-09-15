0f96ce0b07c27008035021b6b47c36368e261830
AJValle <71409214+ValleA242@users.noreply.github.com>
2025-09-15 15:24:12 -0400

    created styling template for search card. Some minor styling changes still need to occur such as background color for the form controls and placehoolder text color

--- inkindggapp/src/components/SearchCard.js
+++ inkindggapp/src/components/SearchCard.js
@@ -1,28 +1,35 @@
 import { Button, Container, Form } from 'react-bootstrap'
 
-function searchContainer() {
-    <div className='d-grid gap-2'>
-        <h1 align='center'> Find Matches before Giving Tuesday at re:Invent</h1>
+function SearchComponent() {
+    return (
+        <Container className='d-grid gap-2 pt-3 text-white' style={{ backgroundColor: "#e78e4c" }}>
 
-        <Form>
-            <Form.Group className="mb-2" controlId='keywordSearch'>
-                <Form.Label>Search</Form.Label>
-                <Form.Control type='search' placeholder='Search (e.g. SPCA, Food Bank)' />
-            </Form.Group>
+            <h1 align='center' className='text-bold'> Find Matches before Giving Tuesday at re:Invent</h1>
 
-            <Form.Group className="mb-2" controlId='orgLocation'>
-                <Form.Label>Location</Form.Label>
-                <Form.Control type='search' placeholder='Location (e.g. Ithica, NY)' />
-            </Form.Group>
+            <Form>
+                <Form.Group className="mb-3" controlId='keywordSearch'>
+                    <Form.Label>Search</Form.Label>
+                    <Form.Control type='search' placeholder='Search (e.g. SPCA, Food Bank)' />
+                </Form.Group>
 
-            <Button variant='primary' type='submit' size='lg'>
-                Search
-            </Button>
+                <Form.Group className="mb-3" controlId='orgLocation'>
+                    <Form.Label>Location</Form.Label>
+                    <Form.Control type='search' placeholder='Location (e.g. Ithica, NY)' />
+                </Form.Group>
 
-            <Button variant='primary' type='submit' size='lg'>
-                Show All
-            </Button>
+                <div className="d-grid">
+                    <Button variant='primary' type='submit' size='lg' className='mb-3' style={{ backgroundColor: "#a2bba8", borderColor: "#a2bba8" }}>
+                        Search
+                    </Button>
 
-        </Form>
-    </div>
+                    <Button variant='primary' type='submit' size='lg' className='mb-3' style={{ backgroundColor: "#a2bba8", borderColor: "#a2bba8" }}>
+                        View All
+                    </Button>
+                </div>
+
+            </Form>
+        </Container>
+    )
 }
+
+export default SearchComponent;
