476e83134a5d7fc71577289af05820a7ddc8c3a9
AJValle <71409214+ValleA242@users.noreply.github.com>
2025-09-16 16:28:34 -0400

    added shopping cart modal

--- inkindggapp/src/components/navbar.js
+++ inkindggapp/src/components/navbar.js
@@ -1,30 +1,47 @@
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav, Modal } from "react-bootstrap";
 import { TiShoppingCart } from "react-icons/ti";
 import { GoSearch } from "react-icons/go";
+import { useState } from "react";
+
 
 
 
 function NavbarComponent() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true);
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                <div className="d-flex align-items-center">
-                    <Nav.Link href="https://www.google.com" className="me-3">
-                        <GoSearch size={25} />
-                    </Nav.Link>
-                    <Nav.Link href="https://www.amazon.com">
-                        <TiShoppingCart size={25} />
-                    </Nav.Link>
-                </div>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <div className="d-flex align-items-center">
+                        <Nav.Link href="https://www.google.com" className="me-3">
+                            <GoSearch size={25} />
+                        </Nav.Link>
+                        <Nav.Link onClick={handleShow}>
+                            <TiShoppingCart size={25} />
+                        </Nav.Link>
+                    </div>
+                </Container>
+            </Navbar>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton></Modal.Header>
+                <Modal.Title>Shopping Cart</Modal.Title>
+                <Modal.Body>
+                    <h1>Populate Amazon Items Here</h1>
+                </Modal.Body>
+
+            </Modal>
+        </>
     );
 }
 
