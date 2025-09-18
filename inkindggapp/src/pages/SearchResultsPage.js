ad42a8655dadb24edca5cc198010468231433172
AJValle <71409214+ValleA242@users.noreply.github.com>
2025-09-18 19:01:38 -0400

    began working on large screen responsiveness. Added search bar to search results page. Links to view all and Show filter toggle. Simple headers for Search and Participating Orgs

--- inkindggapp/src/pages/SearchResultsPage.js
+++ inkindggapp/src/pages/SearchResultsPage.js
@@ -1,7 +1,8 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, FormGroup, Button, Container } from "react-bootstrap";
 import OrgCard from "../components/OrgCard";
 import { organizationArray } from "../OrganizationStore";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 
@@ -9,17 +10,42 @@ function SearchResults() {
 
     return (
         <>
-            <main>
-                <Row xs={1} sm={3} className="g-4">
-
-                    {organizationArray.map((organization) => (
-                        <Col key={organization.id} align="center">
-                            <OrgCard organization={organization} />
+            <main className="containerForSearchContent">
+                <div className="searchFormCont m-5">
+                    <h1 className="searchFormLabel">Search</h1>
+                    <Row as={Form} className="align-items-center justify-content-between g-2">
+                        <Col xs={20} md={7}>
+                            <Form.Group className="searchPageSearchBar mb-0">
+                                <Form.Control type="search" placeholder="Search" />
+                            </Form.Group>
                         </Col>
-                    ))}
 
-                </Row>
-            </main>
+                        <Col xs="auto">
+                            <Link to="#" className="viewAllOrgs p-0">View All</Link>
+                        </Col>
+
+                        <Col xs="auto">
+                            <Button variant="link" className="filterModalToggle p-0">Show Filters</Button>
+                        </Col>
+                    </Row>
+
+                </div>
+
+                <h1 className="searchResultsHeading">Here are some participating organizations!</h1>
+
+                <div className="orgCards">
+
+                    <Row xs={1} sm={3} className="g-4 m-0">
+
+                        {organizationArray.map((organization) => (
+                            <Col key={organization.id} align="center">
+                                <OrgCard organization={organization} />
+                            </Col>
+                        ))}
+
+                    </Row>
+                </div>
+            </main >
         </>
     )
 }
